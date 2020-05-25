56fae45fe9b072fd54625787ac9fd9ab42a33e4f
rufrewp <51385427+rufrewp@users.noreply.github.com>
2020-05-26 01:12:01 +0300

    Add files via upload

---
+++ SharedComponents/TrainersSource.ApplicationServices.General/ApplicationServices/Interfaces/IUseCase.cs
@@ -0,0 +1,14 @@
+﻿using TrainersSource.ApplicationServices.Ports;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainersSource.ApplicationServices.Interfaces
+{
+    public interface IUseCase<in TUseCaseRequest, out TUseCaseResponse>
+        where TUseCaseRequest : IUseCaseRequest<TUseCaseResponse>
+    {
+        Task<bool> Handle(TUseCaseRequest request, IOutputPort<TUseCaseResponse> outputPort);
+    }
+}
