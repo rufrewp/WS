6ea2413b564ffb2c3c59ee374b934335f534ad18
rufrewp <51385427+rufrewp@users.noreply.github.com>
2020-05-26 01:12:55 +0300

    Add files via upload

---
+++ SharedComponents/TrainersSource.BusinessLogic/DomainObjects/Ports/ITrainersRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq.Expressions;
+
+namespace TrainersSource.DomainObjects.Ports
+{
+    public interface IReadOnlyTrainersRepository
+    {
+        Task<Trainers> GetTrainers(long id);
+
+        Task<IEnumerable<Trainers>> GetAllTrainerss();
+
+        Task<IEnumerable<Trainers>> QueryTrainerss(ICriteria<Trainers> criteria);
+
+    }
+
+    public interface ITrainersRepository
+    {
+        Task AddTrainers(Trainers trainers);
+
+        Task RemoveTrainers(Trainers trainers);
+
+        Task UpdateTrainers(Trainers trainers);
+    }
+}
---
+++ SharedComponents/TrainersSource.TrainersListQuery/ApplicationServices/GetTrainersListUseCase/GetTrainersListUseCase.cs
@@ -0,0 +1,37 @@
+﻿using System.Threading.Tasks;
+using System.Collections.Generic;
+using TrainersSource.DomainObjects;
+using TrainersSource.DomainObjects.Ports;
+using TrainersSource.ApplicationServices.Ports;
+
+namespace TrainersSource.ApplicationServices.GetTrainersListUseCase
+{
+    public class GetTrainersListUseCase : IGetTrainersListUseCase
+    {
+        private readonly IReadOnlyTrainersRepository _readOnlyTrainersRepository;
+
+        public GetTrainersListUseCase(IReadOnlyTrainersRepository readOnlyTrainersRepository)
+            => _readOnlyTrainersRepository = readOnlyTrainersRepository;
+
+        public async Task<bool> Handle(GetTrainersListUseCaseRequest request, IOutputPort<GetTrainersListUseCaseResponse> outputPort)
+        {
+            IEnumerable<Trainers> trainerss = null;
+            if (request.TrainersId != null)
+            {
+                var trainers = await _readOnlyTrainersRepository.GetTrainers(request.TrainersId.Value);
+                trainerss = (trainers != null) ? new List<Trainers>() { trainers } : new List<Trainers>();
+
+            }
+            else if (request.Sport != null)
+            {
+                trainerss = await _readOnlyTrainersRepository.QueryTrainerss(new SportCriteria(request.Sport));
+            }
+            else
+            {
+                trainerss = await _readOnlyTrainersRepository.GetAllTrainerss();
+            }
+            outputPort.Handle(new GetTrainersListUseCaseResponse(trainerss));
+            return true;
+        }
+    }
+}
